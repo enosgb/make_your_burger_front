a9df3cdcde4424f495b7991b3aeb4281b29b0687
Enos Gabriel <enosgb55@gmail.com>
2022-11-02 15:39:22 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="principal">
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  },
+  data() {
+    return {
+      logo_src: "./img/logo.png",
+      app_name: "Make Your Burguer",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.main-container {
+  margin-top: 50px;
+  min-height: calc(100vh - 178px);
+  margin-bottom: 0;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
